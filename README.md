94602c5417775f0e4d6fb9fe423fa0d19497ac6f
Iman Langaran <imanlangaran@gmail.com>
2023-11-03 13:22:29 +0330

    commit notes in README

--- README.md
+++ README.md
@@ -2,9 +2,13 @@
 
 TODOs:
 
-- calculate how many micro second it take if you keep pressing the button . so add a delay to that.
-- implement functionality to LEDs.
-- filter remotes.
 - add remote address to EEPROM.
 - connect push button to add new remote.
 - remove remote.
+
+<!-- 
+commit notes:
+added debunce to read command function
+implement functionality to LEDs
+filter remotes 
+-->
--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,94 +1,79 @@
 #include <RCSwitch.h>
 
-short leds[] = {4,5,6,7};
-unsigned long rmAddrs = []; // list of remote addresses
+#define debunce 150
+
+short leds[] = { 4, 5, 6, 7 };
+bool ledStt[] = { 0, 0, 0, 0 };
+unsigned long rmAddrs[10] = { 21532 };
 
 RCSwitch mySwitch = RCSwitch();
 short dec2binWzerofill(unsigned long *Dec);
 
-void setup()
-{
-    Serial.begin(9600);
-    mySwitch.enableReceive(0);
+short i = 0;
+
+void setup() {
+  Serial.begin(9600);
+  mySwitch.enableReceive(0);
+
+  for (i = 0; i < 4; i++) {
+    pinMode(leds[i], OUTPUT);
+    digitalWrite(leds[i], 0);
+  }
+
+  Serial.println("setup...");
 }
 
 short key = 0;
 unsigned long remote_val = 0;
 
-void loop()
-{
-    if (mySwitch.available())
-    {
+long last_sig = 0;
 
-        remote_val = mySwitch.getReceivedValue();
-        key = dec2binWzerofill(&remote_val);
+void loop() {
+  if (mySwitch.available()) {
 
-	//outIndex = leds[key];
+    if (millis() - last_sig > debunce) {
+      last_sig = millis();
 
-        Serial.print("Address: ");
-        Serial.print(remote_val);
-        Serial.print(" - key: ");
-        Serial.println(key);
+      remote_val = mySwitch.getReceivedValue();
+      key = dec2binWzerofill(&remote_val);
 
-	if(is_valid_addr(remote_val)){
-		//do something
-	}
+      if (is_valid_addr(remote_val)) {
+        ledStt[key] = !ledStt[key];
+        digitalWrite(leds[key], ledStt[key]);
+      }
 
-        mySwitch.resetAvailable();
-    }
-}
+      Serial.print("Address: ");
+      Serial.print(remote_val);
+      Serial.print(" - key: ");
+      Serial.println(key);
 
-bool is_valid_addr(unsigned long remoteAddress){
-	for(unsigned long i : rmAddrs){
-		if (i==remoteAddress)
-			return true;
-	}
-	return false;
-}
-
-/*
-short getOutIndex(short key){
-	// key is 1 2 4 8
-
-	//log key in base 2
-
-	//return leds[logkey];
-}*/
-
-short dec2binWzerofill(unsigned long *Dec)
-{
-   // bool cmd[4];
-   // short i = 0;
-
-    short out = 0;
-///////
-    out <<= 1;
-    ((*Dec & 1) > 0) ? out |= 1;
-    *Dec = *Dec >> 1;
-    ((*Dec & 1) > 0) ? out |= 1;
-    *Dec = *Dec >> 1;
-    out <<= 1;
-    ((*Dec & 1) > 0) ? out |= 1;
-    *Dec = *Dec >> 1;
-    ((*Dec & 1) > 0) ? out |= 1;
-    *Dec = *Dec >> 1;
-///////
-/*
-    for (i = 3; i >= 0; i--)
-    {
-        cmd[i] = ((*Dec & 1) > 0) ? 1 : 0;
-        // out <<= 1;
-	// if((*Dec & 1) > 0) out |= 1;
-        *Dec = *Dec >> 1;
+    } else {
+      last_sig = millis();
     }
 
-    for (bool t : cmd)
-    {
-        out <<= 1;
-        out |= t;
-    }*/
 
-    // cmd[4] = '\0';
+    mySwitch.resetAvailable();
+  }
+}
 
-    return out;
+
+bool is_valid_addr(unsigned long remoteAddress) {
+  for (unsigned long i : rmAddrs) {
+    if (i == remoteAddress)
+      return true;
+  }
+  return false;
+}
+
+
+short dec2binWzerofill(unsigned long *Dec) {
+
+  short out = 0;
+
+  for (i = 0; i < 4; i++) {
+    if (*Dec & 1) out = i;
+    *Dec = *Dec >> 1;
+  }
+
+  return out;
 }
