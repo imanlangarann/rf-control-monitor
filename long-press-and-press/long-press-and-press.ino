89292b7b592b03ff13f48e3c552fdea63ae4c4ea
Iman Langaran <imanlangaran@gmail.com>
2023-11-16 18:36:49 +0330

    add long press functionality

---
+++ long-press-and-press/long-press-and-press.ino
@@ -0,0 +1,67 @@
+#define btn 8
+#define debaunce 150
+#define long_press_debaunce 5000
+
+bool btnState = true;
+bool lastBtnState = true;
+bool reading = true;
+
+unsigned long lastDebounceTime = 0;
+
+unsigned long start_press = 0;
+unsigned long startLongPress = 0;
+
+
+void setup() {
+  pinMode(btn, INPUT_PULLUP);
+  Serial.begin(9600);
+  Serial.println("setup...");
+}
+
+void loop() {
+
+  reading = digitalRead(btn);
+
+  if (reading != lastBtnState) {
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debaunce) {
+
+    if (reading != btnState) {
+      btnState = reading;
+      // once its reed this will run
+      // Serial.print("state:");
+
+      if (!btnState) {
+        Serial.println("press");
+        startLongPress = millis();
+      } else {
+        Serial.println("release");
+      }
+    }
+  }
+
+  if (!btnState) {
+    // Serial.print("press");
+    if (millis() - startLongPress > long_press_debaunce) {  // long press
+      Serial.println("long");
+      while (!digitalRead(btn))
+        ;
+      btnState = true;
+      Serial.println("release");
+      // Serial.print(millis() - startLongPress);
+    }
+  }
+
+  lastBtnState = reading;
+
+
+
+  // if(digitalRead(btn) != btnState){
+  //   if(millis() - start_press > debaunce){
+  //     btnState = digitalRead(btn);
+  //   }
+  //   start_press = millis();
+  // }
+}
